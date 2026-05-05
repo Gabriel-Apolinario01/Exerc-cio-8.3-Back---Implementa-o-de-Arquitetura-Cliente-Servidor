565798bee6eb4559ccd385870c836671ec5ce93b
Gabriel-Apolinario01 <107645458+Gabriel-Apolinario01@users.noreply.github.com>
2026-05-05 18:00:37 -0300

    Update route comments and change app host

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from interface.livro_controller import LivroController
 
+# template_folder='.' busca os HTMLs na raiz do projeto
 app = Flask(__name__, template_folder='.')
 CORS(app)
+
 controller = LivroController()
 
-# --- ROTAS DE PÁGINA (FRONTEND) ---
+# --- NAVEGAÇÃO (FRONTEND) ---
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -19,7 +21,7 @@ def pagina_compra():
 def pagina_historico():
     return render_template('compras.html')
 
-# --- ROTAS DE API (BACKEND) ---
+# --- DADOS (BACKEND) ---
 @app.route("/api/livros", methods=['GET'])
 def listar_livros():
     livros_objetos = controller.listar_livros()
@@ -28,7 +30,8 @@ def listar_livros():
 @app.route("/api/comprar/<int:id>", methods=['POST', 'GET'])
 def comprar(id):
     livros = controller.listar_livros()
-    if id < 0 or id >= len(livros): return jsonify({"erro": "Não encontrado"}), 404
+    if id < 0 or id >= len(livros):
+        return jsonify({"erro": "Livro não encontrado"}), 404
     livro = livros[id]
     controller.comprar_livro(livro.nome, livro.preco)
     return jsonify({"mensagem": "Sucesso!", "livro": {"nome": livro.nome, "preco": livro.preco}})
@@ -39,4 +42,4 @@ def listar_compras():
     return jsonify({"historico_compras": [{"nome": c.nome, "preco": c.preco} for c in compras]})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
